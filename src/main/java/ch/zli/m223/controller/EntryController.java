49e894cb16d23be07f967e355e6389f9698405a4
Robin <git+r@obin.ch>
2022-09-10 10:52:06 +0000

    feat: manage entries

---
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -0,0 +1,41 @@
+package ch.zli.m223.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Entry;
+import ch.zli.m223.service.EntryService;
+
+@Path("/entries")
+@Tag(name = "Entries", description = "Handling of entries")
+public class EntryController {
+
+    @Inject
+    EntryService entryService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "List all Entries", description = "Returns all entries.")
+    public List<Entry> list() {
+        return entryService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Add a new Entry", description = "Creates a new entry.")
+    public Entry create(Entry entry) {
+       return entryService.createEntry(entry);
+    }
+
+}
